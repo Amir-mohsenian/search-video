8258912b1ba1ca4aa30fc032e1abaca32bf9a039
amir <amirhosein.mohsenian@rabex.co>
2022-07-14 12:07:12 +0430

    Provide retrofit client

---
+++ app/src/main/java/com/example/filmnet/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.example.filmnet.di
+
+import com.example.filmnet.network.HeaderInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api-v2.filmnet.ir/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        headerInterceptor: HeaderInterceptor
+    ) = OkHttpClient.Builder().addInterceptor(headerInterceptor)
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ) = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient).build()
+
+}
--- app/src/main/java/com/example/filmnet/network/HeaderInterceptor.kt
+++ app/src/main/java/com/example/filmnet/network/HeaderInterceptor.kt
@@ -2,8 +2,9 @@ package com.example.filmnet.network
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import javax.inject.Inject
 
-class HeaderInterceptor: Interceptor {
+class HeaderInterceptor @Inject constructor(): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val requestBuilder = chain.request().newBuilder().addHeader("X-Platform" , "Android")
         return chain.proceed(requestBuilder.build())
